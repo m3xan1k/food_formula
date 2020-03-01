6bdd61f3f647ecd16f417beb63b59751258f42c1
Sergey Shevtsov <dev.serzh@gmail.com>
2020-03-01 10:26:47 +0300

    initialize frontend app with basic config

--- app/app/urls.py
+++ app/app/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('dishes/', include('dishes.urls')),
+    path('', include('dishes.urls')),
+    path('', include('frontend.urls')),
 ]
---
+++ app/frontend/src/components/App.js
@@ -0,0 +1,55 @@
+import React, { Component } from "react";
+import { render } from "react-dom";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: [],
+      loaded: false,
+      placeholder: "Loading"
+    };
+  }
+
+  componentDidMount() {
+    fetch("dishes/dish")
+      .then(response => {
+        if (response.status > 400) {
+          return this.setState(() => {
+            return { placeholder: "Something went wrong!" };
+          });
+        }
+        return response.json();
+      })
+      .then(data => {
+        this.setState(() => {
+          return {
+            data,
+            loaded: true
+          };
+        });
+      });
+  }
+
+  render() {
+    return (
+      <ul>
+        {this.state.data.map(dish => {
+          return (
+            <li key={dish.id}>
+              <h2>{dish.name}</h2>
+              <div>
+                <p>{dish.description}</p>
+              </div>
+            </li>
+          );
+        })}
+      </ul>
+    );
+  }
+}
+
+export default App;
+
+const container = document.getElementById("app");
+render(<App />, container);
