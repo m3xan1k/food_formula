57a811df1ad6f2c049138885ee888b1435165f58
Sergey Shevtsov <dev.serzh@gmail.com>
2020-03-05 23:21:51 +0300

    remove standalone frontend app

--- app/app/urls.py
+++ app/app/urls.py
@@ -21,7 +21,6 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('dishes.urls')),
-    path('', include('frontend.urls')),
 ]
 
 if settings.DEBUG:
