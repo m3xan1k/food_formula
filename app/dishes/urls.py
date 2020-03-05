feb7e91106a75906f9d462e3f74afb8b132c513c
Sergey Shevtsov <dev.serzh@gmail.com>
2020-03-05 23:23:29 +0300

    front url

--- app/dishes/urls.py
+++ app/dishes/urls.py
@@ -1,13 +1,26 @@
 from django.urls import path
-from dishes.views import DishListView, DishView, IngredientListCreateView, TagListCreateView, CategoryListCreateView
+from dishes.views import (
+    DishListView,
+    DishView,
+    IngredientListCreateView,
+    TagListCreateView,
+    CategoryListCreateView,
+)
+from django.views.generic import TemplateView
 
 urlpatterns = [
-    path('dishes/dish/', DishListView.as_view(), name=DishListView.name),
-    path('dishes/dish/<str:slug>/', DishView.as_view(), name=DishView.name),
-    path('dishes/ingredients/', IngredientListCreateView.as_view(),
-         name=IngredientListCreateView.name),
-    path('dishes/tags/', TagListCreateView.as_view(),
-         name=TagListCreateView.name),
-    path('dishes/categories/', CategoryListCreateView.as_view(),
-         name=CategoryListCreateView.name),
+    path('front/', TemplateView.as_view(template_name='dishes/index.html')),
+    path("dishes/dish/", DishListView.as_view(), name=DishListView.name),
+    path("dishes/dish/<str:slug>/", DishView.as_view(), name=DishView.name),
+    path(
+        "dishes/ingredients/",
+        IngredientListCreateView.as_view(),
+        name=IngredientListCreateView.name,
+    ),
+    path("dishes/tags/", TagListCreateView.as_view(), name=TagListCreateView.name),
+    path(
+        "dishes/categories/",
+        CategoryListCreateView.as_view(),
+        name=CategoryListCreateView.name,
+    ),
 ]
